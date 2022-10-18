36b2fcc6a56277abe7d527e5c8894a00d1ffc65a
hwicode <khc3796@naver.com>
2022-10-18 15:41:38 +0900

    hello jpa
    - entityManager find 실습
    - 간단한 JPQL 쿼리 실습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,11 +17,15 @@ public class JpaMain {
         transaction.begin();
 
         try {
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloA");
+//            Member findMember = entityManager.find(Member.class, 1L);
+            List<Member> members = entityManager.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(8)
+                    .getResultList();
 
-            entityManager.persist(member);
+            for (Member member : members) {
+                System.out.println("member.getName() = " + member.getName());
+            }
 
             transaction.commit();
         } catch (Exception e) {
