16ddb8263d9c7068e828d830ce92f9298caca412
hwicode <khc3796@naver.com>
2022-10-18 17:47:36 +0900

    트랜잭션을 지원하는 쓰기 지연

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -18,20 +17,14 @@ public class JpaMain {
 
         try {
 
-            //비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
             //영속
-            System.out.println("==== BEFORE ====");
-            entityManager.persist(member);
-            System.out.println("==== AFTER ====");
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
 
-            Member findMember = entityManager.find(Member.class, 101L);
+            entityManager.persist(member1);
+            entityManager.persist(member2);
 
-            System.out.println("findMember.getName() = " + findMember.getName());
-            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("=======================");
 
             transaction.commit();
         } catch (Exception e) {
