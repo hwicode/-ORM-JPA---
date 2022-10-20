6498def2d4fdd3b45659a40864a16bdf68c4ba11
hwicode <khc3796@naver.com>
2022-10-20 13:12:10 +0900

    엔티티 매핑 실전 예제 1

--- src/main/java/jpashop/JpaStarter.java
+++ src/main/java/jpashop/JpaStarter.java
@@ -1,11 +1,11 @@
-package hellojpa;
+package jpashop;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-public class JpaMain {
+public class JpaStarter {
 
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
@@ -16,20 +16,6 @@ public class JpaMain {
         transaction.begin();
 
         try {
-
-//            Member member = new Member();
-//            member.setUserName("wqewqe");
-//
-//            entityManager.persist(member);
-            Member member = entityManager.find(Member.class, 1L);
-            System.out.println("====================================");
-            entityManager.remove(member);
-            System.out.println("=============================");
-            Member member1 = entityManager.find(Member.class, 1L);
-            System.out.println("member1 = " + member1);
-            System.out.println("member1.getUserName() = " + member1.getUserName());
-            System.out.println("member1.getClass() = " + member1.getClass());
-
             transaction.commit();
         } catch (Exception e) {
             e.printStackTrace();
