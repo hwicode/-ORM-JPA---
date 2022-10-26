56c69057548eea1eb6dbd9a6616e218d2ede6e5b
hwicode <khc3796@naver.com>
2022-10-26 15:23:12 +0900

    객체지향 쿼리 언어1 - 기본 문법 실습

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -23,11 +23,48 @@ public class JpaMain {
             entityManager.flush();
             entityManager.clear();
 
-            entityManager.createQuery("select t from Member m join m.team t", Team.class)
-                    .getResultList();
+            entityManager.createQuery("select m from Member m order by m.age desc", Member.class)
+                            .setFirstResult(1)
+                            .setMaxResults(10)
+                            .getResultList();
 
-            entityManager.createQuery("select new jpql.MemberDto(m.userName, m.age) from Member m", MemberDto.class)
-                    .getResultList();
+            entityManager.createQuery("select m from Member m join m.team t", Member.class)
+                            .getResultList();
+
+            entityManager.createQuery("select count(m) from Member m, Team t where m.userName = t.name", Member.class)
+                            .getResultList();
+
+            entityManager.createQuery("select m, t from Member m left join m.team t on t.name = 'A'", Member.class)
+                            .getResultList();
+
+            entityManager.createQuery("select m, t from Member m left join Team t on m.userName = t.name", Member.class)
+                            .getResultList();
+
+            entityManager.createQuery("select m from Member m where m.age > (select avg(m2.age) from Member m2)", Member.class)
+                            .getResultList();
+
+            entityManager.createQuery("select m from Member m where (select count(o) from Order o where m = o.member) > 0", Member.class)
+                            .getResultList();
+
+            entityManager.createQuery("select m from Member m where exists (select t from m.team t where t.name = 'teamA'")
+                            .getResultList();
+
+            entityManager.createQuery("select o from Order o where o.orderAmount > ALL (select p.stockAmount from Product p)", Order.class)
+                            .getResultList();
+
+            entityManager.createQuery("select m from Member m where m.team = ANY (select t from Team t)", Member.class)
+                            .getResultList();
+
+            String jqpl1 = "select " +
+                            "case when m.age <= 10 then '학생요금' " +
+                            "     when m.age >= 60 then '경로요금' " +
+                            "     else '일반요금' " +
+                            "end " +
+                          "from Member m";
+
+            String jqpl2 = "select coalesce(m.userName, '이름 없는 회원') from Member m";
+
+            String jpql3 = "select NULLIF(m.userName, '관리자') from Member m";
 
             transaction.commit();
         } catch (Exception e) {
