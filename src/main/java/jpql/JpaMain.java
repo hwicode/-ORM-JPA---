56e9c3ead46946f0fde9fe971e30b63ce351dbe8
hwicode <khc3796@naver.com>
2022-10-27 09:55:12 +0900

    벌크 연산

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -28,28 +28,29 @@ public class JpaMain {
             Member member1 = new Member();
             member1.setUserName("member1");
             member1.setTeam(teamA);
+            member1.setAge(0);
             entityManager.persist(member1);
 
             Member member2 = new Member();
             member2.setUserName("member2");
             member2.setTeam(teamA);
+            member2.setAge(0);
             entityManager.persist(member2);
 
             Member member3 = new Member();
             member3.setUserName("member3");
             member3.setTeam(teamB);
+            member3.setAge(0);
             entityManager.persist(member3);
 
-            entityManager.flush();
+            int resultCount = entityManager.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+
             entityManager.clear();
 
-            List<Member> resultList = entityManager.createNamedQuery("Member.findByUserName", Member.class)
-                    .setParameter("userName", "member1")
-                    .getResultList();
-
-            for (Member member : resultList) {
-                System.out.println("member.getUserName() = " + member.getUserName());
-            }
+            Member findMember = entityManager.find(Member.class, member1.getId());
+            System.out.println("resultCount = " + resultCount);
+            System.out.println("findMember.getAge() = " + findMember.getAge());
 
             transaction.commit();
         } catch (Exception e) {
